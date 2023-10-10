eb1804720127ca84378b6c8651cfc0277d7d638b
nicolew88 <nicolew88@nycstudents.net>
2023-10-10 13:17:08 -0400

    .

---
+++ app.js
@@ -0,0 +1,20 @@
+const food = [
+    {
+    name: "apple",
+    color: "red",
+    taste: "sweet",
+    acidity: "moderate",
+    },
+    {
+    name: "banana",
+    color: "yellow",
+    taste: "sweet",
+    acidity: "low",
+    },
+    {
+    name: "",
+    color: "",
+    taste: "",
+    acidity: "",
+    }
+]
