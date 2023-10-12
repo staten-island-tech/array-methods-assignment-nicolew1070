93056632495c676b5ee40c29b8c8faa131c7dd34
nicolew88 <nicolew88@nycstudents.net>
2023-10-12 13:12:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const food = [
     {
     name: "apple",
-    color: "red",
+    type: ["cortland", "golden delicious", "fuji", "jonagold"],
     taste: "sweet",
     acidity: "moderate",
     },
     {
     name: "banana",
-    color: "yellow",
+    type: ["apple banana", "cavendish", "manzano", "mysore"],
     taste: "sweet",
     acidity: "low",
     },
     {
     name: "kiwi",
-    color: "green",
+    type: ["golden", "fuzzy", "artic", "silver vine"],
     taste: "sour",
     acidity: "high",
     },
     {
     name: "grape",
-    color: "green",
+    type: ["cotton candy", "red globe", "concord", "kyoho",],
     taste: "sweet",
     acidity: "moderate",
-    }
-]
+    },
+];
+
+const name = food.filter((food) => food.firstName)
+name.forEach(())
+console.log(name);
